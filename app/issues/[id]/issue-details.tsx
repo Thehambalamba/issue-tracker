653e0807e3411d72142a4dd67de5318e47b797b8
Nikola Pervic <npervic@itekako.com>
2023-10-23 13:38:47 +0200

    Apply the single responsibility principle to <IssueDetailPage />

---
+++ app/issues/[id]/issue-details.tsx
@@ -0,0 +1,27 @@
+import { IssueStatusBadge } from "@/app/components";
+import { Issue } from "@prisma/client";
+import { Heading, Flex, Card, Text } from "@radix-ui/themes";
+import ReactMarkdown from "react-markdown";
+
+type Props = {
+  issue: Issue;
+};
+
+function IssueDetails({
+  issue: { title, status, createdAt, description },
+}: Props) {
+  return (
+    <>
+      <Heading>{title}</Heading>
+      <Flex gap="3" my="2">
+        <IssueStatusBadge status={status} />
+        <Text>{createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose" mt="4">
+        <ReactMarkdown>{description}</ReactMarkdown>
+      </Card>
+    </>
+  );
+}
+
+export default IssueDetails;
