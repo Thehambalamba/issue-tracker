e82dfcc838306722b7ebc9582c451d42b50ec6e4
Nikola Pervic <npervic@itekako.com>
2023-10-24 19:06:02 +0200

    Secure issue creation update and deletion (api and views)

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,10 +1,18 @@
+import authOptions from "@/app/auth/auth-options";
 import { issueSchema } from "@/app/validation-schemas";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 type Params = { params: { id: string } };
 
 export async function PATCH(request: NextRequest, { params: { id } }: Params) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized." }, { status: 401 });
+  }
+
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
 
@@ -34,6 +42,12 @@ export async function PATCH(request: NextRequest, { params: { id } }: Params) {
 }
 
 export async function DELETE(request: NextRequest, { params: { id } }: Params) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized." }, { status: 401 });
+  }
+
   const issue = await prisma.issue.findUnique({
     where: {
       id: +id,
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,12 +4,15 @@ import { notFound } from "next/navigation";
 import EditIssueButton from "./edit-issue-button";
 import IssueDetails from "./issue-details";
 import DeleteIssueButton from "./delete-issue-button";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/auth-options";
 
 type Props = {
   params: { id: string };
 };
 
 async function IssueDetailPage({ params: { id } }: Props) {
+  const session = await getServerSession(authOptions);
   const issue = await prisma.issue.findUnique({ where: { id: +id } });
 
   if (!issue) {
@@ -21,12 +24,14 @@ async function IssueDetailPage({ params: { id } }: Props) {
       <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
-      <Box>
-        <Flex direction="column" gap="4">
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>
+      {session ? (
+        <Box>
+          <Flex direction="column" gap="4">
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      ) : null}
     </Grid>
   );
 }
