5022b89729ed4696d24be5139af25f1a87211eeb
Nikola Pervic <npervic@itekako.com>
2023-10-26 18:24:21 +0200

    Build <LatestIssue />

---
+++ app/latest-issues.tsx
@@ -0,0 +1,50 @@
+import prisma from "@/prisma/client";
+import { Table, Flex, Avatar, Card, Heading } from "@radix-ui/themes";
+import { IssueStatusBadge } from "./components";
+import NextLink from "next/link";
+
+async function LatestIssues() {
+  const issues = await prisma.issue.findMany({
+    orderBy: { createdAt: "desc" },
+    take: 5,
+    include: {
+      assignedToUser: true,
+    },
+  });
+
+  return (
+    <Card>
+      <Heading size="4" mb="5">
+        Latest issues
+      </Heading>
+      <Table.Root>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                <Flex justify="between">
+                  <Flex direction="column" align="start" gap="2">
+                    <NextLink href={`/issues/${issue.id}`}>
+                      {issue.title}
+                    </NextLink>
+                    <IssueStatusBadge status={issue.status} />
+                  </Flex>
+                  {issue.assignedToUser ? (
+                    <Avatar
+                      src={issue.assignedToUser.image!}
+                      fallback={"?"}
+                      size="2"
+                      radius="full"
+                    />
+                  ) : null}
+                </Flex>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </Card>
+  );
+}
+
+export default LatestIssues;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import LatestIssues from "./latest-issues";
+
 export default function Home() {
-  return <div>Home page</div>;
+  return <LatestIssues />;
 }
