1cdb0c59bbedc9e0304bb62285a94d3ffa81cc4c
Nikola Pervic <npervic@itekako.com>
2023-10-26 18:35:55 +0200

    Build <IssueSummary />

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import prisma from "@/prisma/client";
+import IssueSummary from "./issue-summary";
 import LatestIssues from "./latest-issues";
+import { Status } from "@prisma/client";
 
-export default function Home() {
-  return <LatestIssues />;
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: Status.OPEN } });
+  const inProgress = await prisma.issue.count({
+    where: { status: Status.IN_PROGRESS },
+  });
+  const closed = await prisma.issue.count({ where: { status: Status.CLOSED } });
+
+  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
 }
