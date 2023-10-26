aa5d02dbafffcb745fdd06f7480413a8f4b30c71
Nikola Pervic <npervic@itekako.com>
2023-10-26 14:35:30 +0200

    Building <Pagination />

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <div>Hello World</div>;
+import Pagination from "./components/pagination";
+
+type Props = {
+  searchParams: { page: string };
+};
+
+export default function Home({ searchParams: { page } }: Props) {
+  return <Pagination itemCount={100} pageSize={10} currentPage={+page} />;
 }
